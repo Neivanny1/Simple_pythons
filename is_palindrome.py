b867efa6559515417550ebb00ed8710e0aeb9afd
Neivanny1 <nevileneivanny@gmail.com>
2023-12-15 01:17:20 +0300

    checking for anagrams

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,10 +1,20 @@
-# Checks if string is palindrome
+# Checks if string is palindrome,anagram
+from collections import Counter
+
+
 def is_palindrome(word):
     rev_word = word[::-1]
     if (word == rev_word):
         print(f'{word} is palindrome')
     else:
         print(f'{word} is not palindrome')
-    
+        
+def is_anagram(word1, word2):
+    if Counter(word1) == Counter(word2):
+        print(f'{word1} and {word2} are anagrams')
+    else:
+        print(f'{word1} and {word2} are not anagrams')
+        
 if __name__ == "__main__":
-    is_palindrome('eric')
+    #is_palindrome('eric')
+    is_anagram('save', 'evas')
